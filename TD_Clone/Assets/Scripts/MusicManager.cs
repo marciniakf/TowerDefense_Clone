818aa6cff30095a1f0b36bac7a23633c27f69e03
Filip <marciniak.f@gmail.com>
2017-11-15 23:01:43 +0100

    UI+MusicManager

---
+++ TD_Clone/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audiosource;
+
+	void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+		Debug.Log("Dont destroy on load: " + name);
+	}
+
+	void Start()
+	{
+		audiosource = GetComponent<AudioSource>();
+	}
+
+	void OnLevelWasLoaded(int level)
+	{
+
+		AudioClip thisLevelMusic = levelMusicChangeArray[level];
+		Debug.Log("Playing clip: " + thisLevelMusic);
+
+		if (thisLevelMusic) // jeżeli jest tu muzyka
+		{
+			audiosource.clip = thisLevelMusic;
+			audiosource.loop = true;
+			audiosource.Play();
+		}
+	}
+}
+	
+	
