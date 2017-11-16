79b5fad2db6ba24f89ed327bb8fb07e2f7f807f6
Filip <marciniak.f@gmail.com>
2017-11-16 03:12:15 +0100

    update_1

---
+++ TD_Clone/Assets/Scripts/FadeIn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour {
+
+	public float FadeInTime;
+
+	private Image fadePanel;
+	private Color currentColor = Color.black;
+
+
+
+	void Start () {
+		fadePanel = GetComponent<Image>();
+		
+	}
+	
+
+	void Update () {
+		if (Time.timeSinceLevelLoad < FadeInTime) {
+
+			float AlphaChange = Time.deltaTime / FadeInTime;
+			currentColor.a -= AlphaChange;
+			fadePanel.color = currentColor;
+		}
+		else {
+			gameObject.SetActive(false);
+		}
+	}
+}
